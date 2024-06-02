e94768ffe4e12631f9e93a2a938dc48d5c414577
ShinjiMC <nayapbrau@gmail.com>
2024-06-02 02:29:22 -0500

    backend: Refactor AnalyzePose

--- video.cpp
+++ video.cpp
@@ -8,46 +8,31 @@ using namespace std;
 int main()
 {
     VideoCapture cap;
-    
-    // Intentar abrir la cámara frontal (usualmente 1)
-    if (!cap.open(1))
+    if (!cap.open(0)) // Intentar abrir la cámara frontal (usualmente 1)
     {
-        // Si no se puede abrir la cámara frontal, usar la cámara por defecto (0)
         if (!cap.open(0))
         {
             cout << "Error al abrir la cámara!" << endl;
             return -1;
         }
     }
-
-    // Crear una ventana
     namedWindow("Webcam", WINDOW_AUTOSIZE);
-
     while (true)
     {
         Mat frame;
-        // Capturar el frame
         cap >> frame;
         if (frame.empty())
         {
             cout << "No se puede capturar el frame!" << endl;
             break;
         }
-
-        // Mostrar el frame en la ventana
         imshow("Webcam", frame);
-
-        // Esperar 30 ms y salir si se presiona la tecla 'q'
-        if (waitKey(30) == 'q')
+        if (waitKey(30) == 'q') // Esperar 30 ms y salir si se presiona la tecla 'q'
         {
             break;
         }
     }
-
-    // Liberar el objeto VideoCapture
     cap.release();
-    // Destruir todas las ventanas
     destroyAllWindows();
-
     return 0;
 }
